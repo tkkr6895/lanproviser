a5bfd467ff8910ddffde82cb18f69029e4de84d9
Trishal Kumar <tkkr6895@gmail.com>
2014-11-16 14:36:46 +0530

    Mini-Project completion commit

--- src/main/java/lanproviser/lanproviser.java
+++ src/main/java/lanproviser/lanproviser.java
@@ -1,28 +1,26 @@
 package lanproviser;
 import java.io.IOException;
 import java.util.List;
-import java.util.Scanner;
 
 import org.languagetool.JLanguageTool;
 import org.languagetool.language.BritishEnglish;
 import org.languagetool.rules.RuleMatch;
 
-public class lanproviser {
+class lanproviser {
 
-	public static void main(String[] args) throws IOException{
+	public void grammar(String moo) throws IOException{
 		// TODO Auto-generated method stub
-		Scanner moo = new Scanner(System.in);
+
 		JLanguageTool langTool = new JLanguageTool(new BritishEnglish());
 		langTool.activateDefaultPatternRules();
-		System.out.println("Enter text for proof reading plis");
-		String input = moo.nextLine();
-		List<RuleMatch> matches = langTool.check(input);
+		List<RuleMatch> matches = langTool.check(moo);
 		 for (RuleMatch match : matches) 
 		{
-		  System.out.println("YUDODIS, Plis check for error at" +
+		  System.out.println("Errors found at" +
 		  match.getLine() + ", column " +match.getColumn() + ": " + match.getMessage());
-		  System.out.println("Corrections suggested: " +match.getSuggestedReplacements());
+		  System.out.println("Suggested correction " +match.getSuggestedReplacements());
 		}
+		 
 	}
 
 }
